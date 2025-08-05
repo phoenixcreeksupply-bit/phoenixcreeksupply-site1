cd68901069f35bcd41c3e31224e6dc5b295ac9ed
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-08-05 08:40:50 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-white text-black p-4">
+      <h1 className="text-4xl font-bold mb-2">Welcome to Phoenix Creek Supply</h1>
+      <p className="text-lg text-gray-600">Rugged Tools for the Modern Stoic.</p>
+    </main>
+  );
+}
