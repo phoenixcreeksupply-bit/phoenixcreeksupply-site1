309c35a68c6519c9c3a2f4bf0af4ff1aaf8b6c76
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-08-05 19:20:56 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#1F2937",
-        accent: "#CA8A04"
-      }
-    }
-  },
-  plugins: []
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 }
