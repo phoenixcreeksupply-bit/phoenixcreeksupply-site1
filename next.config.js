fc411765ae8d1bf1a1d5092a4fe2233bccdafe10
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-08-09 00:10:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' },
+    ],
+  },
+};
+module.exports = nextConfig;
