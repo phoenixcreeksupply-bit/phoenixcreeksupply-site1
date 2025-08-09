ebb277067534f3d95085396fd8a8809a9db8f658
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-08-08 22:03:41 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 // pages/_app.js
-import '../styles/globals.css';
+import "../styles/globals.css";
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="min-h-screen bg-white text-gray-900 font-sans">
+      <Header />
+      <div className="px-6 pb-12">
+        <Component {...pageProps} />
+      </div>
+    </main>
+  );
 }
